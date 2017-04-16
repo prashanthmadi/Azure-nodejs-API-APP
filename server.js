4982c66d660a37d85c71b162394b08a8ee0d236d
prashanthmadi <prashanthrmadi@gmail.com>
2017-04-15 21:53:31 -0500

    adding swagger-ui

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var Express = require('express');
 var BodyParser = require('body-parser');
 var Swaggerize = require('swaggerize-express');
 var Path = require('path');
+var swaggerUi = require('swaggerize-ui');
 
 var App = Express();
 
@@ -21,7 +22,11 @@ App.use(Swaggerize({
     handlers: Path.resolve('./handlers')
 }));
 
-Server.listen(process.env.PORT||8000, function () {
+App.use('/docs', swaggerUi({
+    docs: '/v1/api-docs' // from the express route above.
+}));
+
+Server.listen(process.env.PORT || 8000, function () {
     App.swagger.api.host = this.address().address + ':' + this.address().port;
     /* eslint-disable no-console */
     console.log('App running on %s:%d', this.address().address, this.address().port);
